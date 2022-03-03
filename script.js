799275c9eb73bae4678e4d8dbd92464962e9412e
Mironshoh <mironshohasadov1602@gmail.com>
2022-03-03 11:56:37 +0500

    Fix New Game button

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 'use strict';
 
+// Random function start
 function randomFunc() {
   let randomNum = Math.trunc(Math.random() * 6) + 1;
   return randomNum;
 }
+// Random function finish
 
 let swapNum = 1;
 let score = Number(
   document.querySelector(`#current--${(swapNum + 1) % 2}`).textContent
 );
 
+// Roll button click start
 document.querySelector('.btn--roll').addEventListener('click', function () {
   let nowRandom = randomFunc();
 
@@ -22,6 +25,9 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   }
   document.querySelector(`#current--${(swapNum + 1) % 2}`).textContent = score;
 });
+// Roll button click start
+
+// Hold function start
 function holdFunc() {
   let playerOneScore = Number(
     document.querySelector(`#score--${(swapNum + 1) % 2}`).textContent
@@ -31,36 +37,57 @@ function holdFunc() {
 
   document.querySelector(`#score--${(swapNum + 1) % 2}`).textContent =
     playerOneScore;
-  document
-    .querySelector(`.player--${swapNum % 2}`)
-    .classList.add('player--active');
-  document
-    .querySelector(`.player--${(swapNum + 1) % 2}`)
-    .classList.remove('player--active');
-  swapNum++;
+
   const nodeList = document.querySelectorAll('.current-score');
   for (let i = 0; i < nodeList.length; i++) {
     nodeList[i].textContent = score;
   }
+  swapNum++;
 
-  if (playerOneScore > 99) {
-    document.querySelector(`.player--${swapNum % 2}`).style.backgroundColor =
-      '#07c707';
-    document.querySelector(
-      `.player--${(swapNum + 1) % 2}`
-    ).style.backgroundColor = '#fc3737f6';
+  if (playerOneScore > 9) {
+    document
+      .querySelector(`.player--${swapNum % 2}`)
+      .classList.add('backgreen');
+    document
+      .querySelector(`.player--${(swapNum + 1) % 2}`)
+      .classList.add('backred');
     let btnList = document.querySelectorAll('.btn');
     for (let i = 0; i < btnList.length; i++) {
       btnList[i].disabled = true;
     }
     document.querySelector('.btn--new').disabled = false;
+  } else {
+    document
+      .querySelector(`.player--${swapNum % 2}`)
+      .classList.remove('backgreen');
+    document
+      .querySelector(`.player--${(swapNum + 1) % 2}`)
+      .classList.remove('backred');
+    document
+      .querySelector(`.player--${swapNum % 2}`)
+      .classList.remove('backred');
+    document
+      .querySelector(`.player--${(swapNum + 1) % 2}`)
+      .classList.remove('backgreen');
+    console.log('Hello1');
+    document
+      .querySelector(`.player--${swapNum % 2}`)
+      .classList.add('player--active');
+    document
+      .querySelector(`.player--${(swapNum + 1) % 2}`)
+      .classList.remove('player--active');
+    console.log(swapNum);
   }
 }
+// Hold function finish
 
+// Hold button click start
 document.querySelector('.btn--hold').addEventListener('click', function () {
   holdFunc();
 });
+// Hold button click finish
 
+// New button click start
 document.querySelector('.btn--new').addEventListener('click', function () {
   let btnList = document.querySelectorAll('.btn');
   for (let i = 0; i < btnList.length; i++) {
@@ -75,6 +102,7 @@ document.querySelector('.btn--new').addEventListener('click', function () {
     currentList[i].textContent = '0';
   }
   document.querySelector('.dice').src = `dice-5.png`;
-  swapNum = 2;
+  swapNum = 1;
   holdFunc();
 });
+// New button click finish
